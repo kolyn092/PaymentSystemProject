8d91623e6e5e76cf3a33a7e4036a4716ee101fad
kolyn092 <kolyn092@gmail.com>
2026-05-28 23:39:07 +0900

    first commit

---
+++ src/main/java/com/paymentsystemproject/PaymentSystemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.paymentsystemproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentSystemProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentSystemProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paymentsystemproject/PaymentSystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentsystemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentSystemProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
