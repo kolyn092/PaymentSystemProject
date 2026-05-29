e6208702e0ed03396f99f8816609b1528e60dc84
kolyn092 <kolyn092@gmail.com>
2026-05-29 20:43:34 +0900

    feat: base entity 관련 수정

--- src/main/java/com/paymentsystemproject/PaymentSystemProjectApplication.java
+++ src/main/java/com/paymentsystemproject/PaymentSystemProjectApplication.java
@@ -2,12 +2,14 @@ package com.paymentsystemproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class PaymentSystemProjectApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(PaymentSystemProjectApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentSystemProjectApplication.class, args);
+	}
 
 }
--- src/main/java/com/paymentsystemproject/global/entity/BaseTimeEntity.java
+++ src/main/java/com/paymentsystemproject/global/entity/BaseTimeEntity.java
@@ -2,16 +2,24 @@ package com.paymentsystemproject.global.entity;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
-@MappedSuperclass
 @Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
-	@Column(name = "created_at")
+	@CreatedDate
+	@Column(name = "created_at", nullable = false, updatable = false)
 	private LocalDateTime createdAt;
 
-	@Column(name = "updated_at")
+	@LastModifiedDate
+	@Column(name = "updated_at", nullable = false)
 	private LocalDateTime updatedAt;
 }
