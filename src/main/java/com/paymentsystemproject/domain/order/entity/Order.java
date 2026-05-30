79896afe9446b4f7d5bec05f0ea2037e4f5d51a2
정예진 <yxejxnn@gmail.com>
2026-05-30 17:18:42 +0900

    feat: OrderItem enum 추가 및 Order, OrderItem 엔티티 수정

--- src/main/java/com/paymentsystemproject/domain/order/entity/Order.java
+++ src/main/java/com/paymentsystemproject/domain/order/entity/Order.java
@@ -1,8 +1,12 @@
 package com.paymentsystemproject.domain.order.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.paymentsystemproject.domain.member.entity.Member;
 import com.paymentsystemproject.global.entity.BaseTimeEntity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -11,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
@@ -44,5 +49,15 @@ public class Order extends BaseTimeEntity {
     private Integer totalAmount;
 
     @Column(length = 20, nullable = false)
-    private String status;
+    private OrderStatus status;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    public Order(Member member, String orderNumber, Integer totalAmount) {
+        this.member = member;
+        this.orderNumber = orderNumber;
+        this.totalAmount = totalAmount;
+        this.status = OrderStatus.PENDING_PAYMENT;
+    }
 }
