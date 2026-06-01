2ca1fe13f697cf07a3ce02faf27d66fea82d4e01
devdong1231 <79241089+devdong1231@users.noreply.github.com>
2026-06-01 16:03:51 +0900

    feat: 결제 생성 메서드 작성, 관련된 ErrorCode 작성
    # Conflicts:
    #	src/main/java/com/paymentsystemproject/global/error/ErrorCode.java

--- src/main/java/com/paymentsystemproject/domain/payment/controller/PaymentController.java
+++ src/main/java/com/paymentsystemproject/domain/payment/controller/PaymentController.java
@@ -1,15 +1,17 @@
 package com.paymentsystemproject.domain.payment.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.paymentsystemproject.domain.payment.service.PaymentService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class PaymentController {
 
-	private final PaymentService paymentService;
+    private final PaymentService paymentService;
 
-	public PaymentController(PaymentService paymentService) {
-		this.paymentService = paymentService;
-	}
 }
