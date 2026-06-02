bc2e037be19aa08e1c04627859bbc17c30a712a9
devdong1231 <79241089+devdong1231@users.noreply.github.com>
2026-06-02 10:50:10 +0900

    feat: 결제 대행사 포트 인터페이스 설계, PortOne Adapter(PortOneClient) 작성

---
+++ src/main/java/com/paymentsystemproject/domain/infra/portone/client/PortOneClient.java
@@ -0,0 +1,50 @@
+package com.paymentsystemproject.domain.infra.portone.client;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+import com.paymentsystemproject.domain.infra.portone.config.PortOneProperties;
+import com.paymentsystemproject.domain.infra.portone.dto.PortOneCancelRequest;
+import com.paymentsystemproject.domain.infra.portone.dto.PortOnePaymentResponse;
+import com.paymentsystemproject.domain.payment.port.PaymentGateway;
+import com.paymentsystemproject.domain.payment.port.PaymentGatewayResponse;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class PortOneClient implements PaymentGateway {
+
+    private final RestClient portOneRestClient;
+    private final PortOneProperties portOneProperties;
+
+    @Override
+    public PaymentGatewayResponse getPayment(String paymentId) {
+        // paymentId logging
+        log.info("PortOne 결제 조회: {}", paymentId);
+
+        // portOneRestClient https://api.portone.io/payments/{paymnetId}?storeId={storeId}
+        PortOnePaymentResponse response = portOneRestClient.get()
+            .uri(uriBuilder -> uriBuilder.path("/payments/{paymentId}")
+                .queryParam("storeId", portOneProperties.getStoreId())
+                .build(paymentId))
+            .retrieve()
+            .body(PortOnePaymentResponse.class);
+
+        // PortOne 응답 결과인 PortOnePaymentResponse를 PaymentGatewayResponse로 변환
+        return new PaymentGatewayResponse(response.id(), response.status(), response.amount().total());
+    }
+
+    @Override
+    public void cancelPayment(String paymentId, String reason, Integer amount) {
+        log.info("PortOne 결제 취소 요청: paymentId={}, amount={}, reason={}", paymentId, amount, reason);
+
+        portOneRestClient.post()
+            .uri("/payments/{paymentId}/cancel", paymentId)
+            .body(new PortOneCancelRequest(reason, amount, null, portOneProperties.getStoreId()))
+            .retrieve()
+            .toBodilessEntity();
+    }
+}
