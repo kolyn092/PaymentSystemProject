e4652a4cb81c45decba527d7269cd8ac71ec760d
정예진 <yxejxnn@gmail.com>
2026-06-04 15:11:47 +0900

    fix: Order status 필드 @Enumerated(EnumType.STRING) 추가

--- src/main/java/com/paymentsystemproject/domain/order/entity/Order.java
+++ src/main/java/com/paymentsystemproject/domain/order/entity/Order.java
@@ -9,6 +9,8 @@ import com.paymentsystemproject.global.entity.BaseTimeEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -49,6 +51,7 @@ public class Order extends BaseTimeEntity {
     private Integer totalAmount;
 
     @Column(length = 20, nullable = false)
+    @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
